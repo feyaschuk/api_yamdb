02cf7575dcc86251fe59a2ea984b7de35f41a4eb
DmitriiPugachev <pugachev.feddebate@gmail.com>
2021-08-10 17:45:19 +0200

    Check

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    DEFAULT_USER = 'user'
+    DEFAULT_USER = 'user-custom'
     USER_STATUSES = [
         ('user', 'user'),
         ('moderator', 'moderator'),
