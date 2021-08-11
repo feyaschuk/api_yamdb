f05526d690ebbda476af976efef53f5c01c157ff
Mary Yaschuk <feyaschuk@mail.ru>
2021-08-11 10:40:08 +0300

    added #comments to models.py about name chaging

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -9,7 +9,8 @@ class User(AbstractUser):
         ('moderator', 'moderator'),
         ('admin', 'admin')
     ]
-    user_status = models.CharField(max_length=150,
+    user_status = models.CharField(max_length=150, # при миграции выдавал 
+    # ошибку, обяз.параметр макс.длина поля. поэтому добавила.
         choices=USER_STATUSES,
         default=DEFAULT_USER
     )
@@ -20,27 +21,29 @@ class User(AbstractUser):
 
 
 class Category(models.Model):
+    # изменила имя с title на name, в redoc вывод поля name указан.
     name = models.CharField(max_length=200,
                              help_text='Укажите название категории')
     slug = models.SlugField(max_length=150, unique=True,
                             verbose_name='Идентификатор категории',)
 
     class Meta:
+        # изменила имя с title на name, в redoc вывод поля name указан.
         ordering = ('name',)
         verbose_name_plural = 'Категория'
 
     def __str__(self):
-        return self.name
+        return self.name # изменила имя с title на name, в redoc вывод поля name указан.
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200,
+    name = models.CharField(max_length=200,# изменила имя с title на name, в redoc вывод поля name указан.
                              help_text='Укажите жанр')
     slug = models.SlugField(max_length=150, unique=True, blank=True, null=True,
                             verbose_name='Идентификатор жанра',)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ('name',)# изменила имя с title на name, в redoc вывод поля name указан.
         verbose_name_plural = 'Жанр'
 
     def __str__(self):
@@ -48,13 +51,13 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=200,)
+    name = models.CharField(max_length=200,)# изменила имя с title на name, в redoc вывод поля name указан.
     year = models.IntegerField(verbose_name="Год выпуска")
     description = models.CharField(max_length=200, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True,
                                  related_name='titles_category',
                                  verbose_name='Категория')
-    genre = models.ManyToManyField(Genre, verbose_name='Жанр', blank=True)
+    genre = models.ManyToManyField(Genre, verbose_name='Жанр', blank=True) # изменила имя с titles на title(как у тебя раньше было).
 
     class Meta:
         ordering = ('id',)
