c8b0230baf63fac13b703aeba91a8a038ec870da
BigStanRus <Klimenko_sasha32@mail.ru>
2021-08-11 16:00:13 +0500

    Create serializers for Title,Genre,Category. Create csv_to_sql_v1

---
+++ api_yamdb/api/csv_to_cql.py
@@ -0,0 +1,20 @@
+# Шаблон для 1 таблицы (category.csv)
+import csv
+import sqlite3
+
+conn= sqlite3.connetct("db.sqlite3")
+c = conn.cursor()
+
+with open("static/data/category.csv", "r") as data:
+    reader = csv.DictReader(data)
+    for row in reader:
+        id = row['id']
+        name = row['name']
+        slug = row['slug']
+        c.execute(
+            "INSERT INTO reviews_category "
+            "(id, name, slug) "
+            "VALUES(?, ?, ?)",
+            (id, name, slug)
+        )
+        conn.commit()
