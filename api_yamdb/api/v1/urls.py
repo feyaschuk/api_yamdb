f119c38659a6b29230ace37b9b3bf9fab714acaf
Mary Yaschuk <feyaschuk@mail.ru>
2021-08-11 12:16:24 +0300

    fix mistake in viewsets, v1.urls, change fieldtype score in Review model

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,17 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet, ReviewViewSet, UserViewSet
+from api.v1.views import CommentViewSet, ReviewViewSet, UserViewSet
 
 router_v1 = DefaultRouter(trailing_slash='optional')
 router_v1.register('users/?', UserViewSet)
-router_v1.register('titles/?', ReviewViewSet)
 router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews/?',
-    CommentViewSet, basename="comments")
+    r'titles/(?P<titles_id>\d+)/reviews/?',
+    ReviewViewSet, basename="reviews")
+
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/?',
-    ReviewViewSet, basename="reviews")
+    CommentViewSet, basename="comments")
 
 
 urlpatterns = [
--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -6,19 +6,18 @@ from rest_framework.pagination import LimitOffsetPagination
 
 from .serializers import (CommentSerializer, ReviewSerializer,UserSerializer)
 
-class ReviewViewSet(viewsets.ModelViewSet):
-    
-    queryset = Review.objects.all()
+class ReviewViewSet(viewsets.ModelViewSet):    
     serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
     
     def perform_create(self, serializer):
-        title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
-        serializer.save(author=self.request.user, title=title)
+        title = get_object_or_404(Title, pk=self.kwargs.get("titles_id"))
+        serializer.save(title=title)
 
     def get_queryset(self):
-        review_id = self.kwargs.get("review_id")
-        review = get_object_or_404(Review, pk=review_id)
-        return review.comments.all()
+        title_id = self.kwargs.get("titles_id")
+        title = get_object_or_404(Title, pk=title_id)
+        return Review.objects.filter(title=title)
     
 class CommentViewSet(viewsets.ModelViewSet):    
     serializer_class = CommentSerializer
@@ -36,3 +35,4 @@ class UserViewSet(viewsets.ModelViewSet):
     
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
