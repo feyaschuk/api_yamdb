fd8df4b20905f30e08d9cd1140589b2be4c13446
BigStanRus <Klimenko_sasha32@mail.ru>
2021-08-12 23:14:42 +0500

    Попытка №3

---
+++ api_yamdb/api/v1/serializers.py
@@ -0,0 +1,77 @@
+from reviews.models import (Comment, Review, User,
+                            Title, Genre, Category)
+from rest_framework import serializers
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        fields = '__all__'
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        fields = '__all__'
+        model = Comment    
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = User
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('title', 'slug')
+        model = Category
+        lookup_field = 'slug'
+        extra_kwargs = {
+            'url': {'lookup_field': 'slug'}
+        }
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('title', 'slug')
+        model = Genre
+        lookup_field = 'slug'
+        extra_kwargs = {
+            'url': {'lookup_field': 'slug'}
+        }
+
+
+class RepresentCategory(serializers.SlugRelatedField):
+    def to_representation(self, obj):
+        serializer = CategorySerializer(obj)
+        return serializer.data
+
+
+class RepresentGenre(serializers.SlugRelatedField):
+    def to_representation(self, obj):
+        serializer = GenreSerializer(obj)
+        return serializer.data
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = RepresentCategory(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+        required=False
+    )
+    genre = RepresentGenre(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True
+    )
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'rating',
+                  'description', 'genre', 'category')
+        model = Title
