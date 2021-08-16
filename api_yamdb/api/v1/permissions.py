94c910a13bb005334e0b718c72a42a4d60dccfbf
Mary Yaschuk <feyaschuk@mail.ru>
2021-08-16 13:38:48 +0300

    optimise permission code

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -2,35 +2,30 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsModeratorOrAdminOrReadOnly(BasePermission):    
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
+    def has_permission(self, request, view):        
         if request.user.is_authenticated:
             if request.method=="POST" or request.method=="PATCH" or request.method=='DELETE':
                 return True
             elif (request.user.role=='admin' 
             or request.user.role=='moderator' or request.user.is_superuser):
                 return True
+        return request.method in SAFE_METHODS
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        else:
-            if request.method=="PATCH" or request.method=='DELETE':
-                if obj.author == request.user or (request.user.role=='admin' 
-            or request.user.role=='moderator' or request.user.is_superuser):  
-                    return True
-             
+        if request.method=="PATCH" or request.method=='DELETE':
+            if obj.author == request.user or (request.user.role=='admin' 
+        or request.user.role=='moderator' or request.user.is_superuser):  
+                return True
+        return request.method in SAFE_METHODS 
+
 
 class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
+    def has_permission(self, request, view):        
         if request.user.is_authenticated:
             if request.user.role=='admin' or request.user.is_superuser:
                 return True
-    
+        return request.method in SAFE_METHODS
 
 class IsAdminOrSuperUser(BasePermission):
     def has_permission(self, request, view):
